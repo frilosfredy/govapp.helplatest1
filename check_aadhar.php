0601a57580da568793ea1564de915371353ade3f
frilosfredy <126310279+frilosfredy@users.noreply.github.com>
2024-11-27 11:26:15 +0530

    check_aadhar.php

---
+++ check_aadhar.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Check Aadhaar pan card linked Status</title>
+     <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+            background-color: #f9f9f9;
+        }
+
+        .main-content {
+            max-width: 1000px;
+            margin: 0px auto;
+            background: #fff;
+            padding: 20px;
+            border: 1px solid #ddd;
+            box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+            
+        }
+
+        .content-section ul {
+            padding: 0;
+            list-style: none;
+        }
+
+        .content-section ul li {
+            margin: 10px 0;
+        }
+
+        .content-section ul li strong {
+            color: #28527a;
+        }
+
+        .content-section img {
+            display: block;
+            margin: 20px 0;
+            width: 800px;
+            height: auto;
+        } 
+        .content-section h1{
+            color:red;
+        }
+    </style>
+</head>
+<body>
+    
+        <!-- Content for Aadhaar Section -->
+        <div id="check_aadhar.php" class="content-section">
+            <h1> Check Aadhaar pan card linked Status</h1>
+            <h2> Online Method via Income Tax e-Filing Portal</h2>
+            <ul>
+            <li><strong>Step 1:</strong> Visit the <a href="https://www.incometax.gov.in/iec/foportal/" target="_blank">official E filling website</a>.</li>
+                <li><strong>Step 2:</strong> Find the "Link Aadhaar" Option:On the homepage, look for the <strong>"Link Aadhaar"</strong> option in the <strong>Quick Links</strong> section.</p>
+                </li>
+                <li><strong>Step 3:</strong> Enter the details.</li>
+                <li><strong>Step 4:</strong> Enter your <strong>PAN number</strong> and <strong>Aadhaar number</strong> in the provided fields.</li>
+                <img src="http://localhost/govapphelper/guides/aadhar/_images/pic2.png" alt="Screenshot of the Aadhaar PAN card status"/>
+                <li><strong>Step 5:</strong>Click on the <strong>"View Link Aadhaar Status"</strong> button.</li>
+                <li><strong>Step 6:</strong>View Status:</strong></li>
+                <li><strong>Step 7:</strong>If your Aadhaar is linked to PAN, you will see a confirmation message.</strong></li>
+                <li><strong>step 8:</strong>If it is not linked, you will be prompted to complete the linking process.</strong></li>
+            </ul>
+        </div>
+    
+</body>
+</html>
